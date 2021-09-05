fcaa1cb8255a4f97f79bd520b6b3d78500f196ce
yewonbahn <banyaewon98@gmail.com>
2021-09-05 23:01:26 +0900

    9월 5일 -ing

---
+++ 0903- sort/1316.cpp
@@ -0,0 +1,49 @@
+//
+// Created by banya on 2021-09-05.
+//
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+vector<string> arr;
+
+int bubbleSort(string arr2) {
+    int cnt = 0;
+
+    for (int i = 0; i < arr2.length(); i++) {
+
+        for (int j = 1; j < arr2.length() ; j++) {
+            if ((arr2[i] == arr2[j] )& (arr2[i] != arr2[j-1] ) &(j-i>0)) {
+
+                cnt++;
+            }
+
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    int n;
+    int cnt2;
+    cnt2 = 0;
+    //입력
+    cin >> n;
+    arr.assign(n, "");
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i]  ;
+
+    //연산
+//    sort(a.begin(), a.end(), cmpAdv);
+    for (int i = 0; i < n; i++)
+        if(bubbleSort(arr[i]) > 0)
+            cnt2++;
+
+
+    cout << cnt2 << " ";
+
+}
