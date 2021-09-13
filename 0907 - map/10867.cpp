bf8a41d880d9ea05cd7c42b39794da311480dce4
yewonbahn <banyaewon98@gmail.com>
2021-09-13 11:51:31 +0900

    9월 13일 -Update

--- 0907 - map/10867.cpp
+++ 0907 - map/10867.cpp
@@ -3,19 +3,43 @@
 //
 
 #include <iostream>
-#include <set>
 
+#include <math.h>
+#include <map>
 using namespace std;
 
 int main(){
-    int n, k ;
-    set<int> s;
+    int n1, n2,n3 ;
+    int m1, m2,m3 ;
+    int k;
 
-    cin >> n;
-    while (n--){
-        cin >> k;
-        s.insert(k);
+    string ans;
+    cin >> n1 >> n2 >> n3;
+    cin >> m1 >> m2 >> m3;
+
+    if(n1-(n2+m3-m2)*m1<=0)
+        cout << "Danger Diet";
+    else
+        cout << n1-(n2+m3-m2)*m1 <<" "<< n2;
+    k = n1-(n2+m3-m2)*m1;
+    while(m1--){
+
+        if(n2+m3-m2 > n3) {
+            n1= n1-(n2+m3-m2);
+            k = floor((n2 + m3 - m2) / 2);
+            n2 = n2 - k;
+
+        }
+        else
+            n1= n1-(n2+m3-m2);
     }
-    for (auto iter = s.begin();iter!=s.end();iter++)
-        cout << *iter << " ";
+    cout << "\n";
+    if(n1>k)
+        ans="YOYO";
+    else
+        ans="NO";
+    if(n1<=0 || n2 <=0)
+        cout << "Danger Diet";
+    else
+        cout << n1 <<" "<< n2 <<" "<<ans;
 }
