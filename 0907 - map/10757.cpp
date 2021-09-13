f69496271f658a3ba71eee9b4ed2e45c0813b080
yewonbahn <banyaewon98@gmail.com>
2021-09-13 19:15:27 +0900

    9월 13일 -ing

--- 0907 - map/10757.cpp
+++ 0907 - map/10757.cpp
@@ -13,7 +13,45 @@
 using namespace std;
 
 int main(){
-    long long n1,n2;
-    cin >> n1 >> n2;
-    cout << n1+n2;
+    string n,n2,n4;
+    int large;
+    int a = 0;
+    int ans=0;
+    int ans2=0;
+    n4="";
+
+
+    cin >> n >> n2;
+
+    if(n.length()>n2.length()){
+        string n3;
+        large = n.length();
+        a= large-n2.length();
+        for(int i=0;i<a;i++)
+            n3 = n3 +"0";
+        n2=n3+n2;
+    }
+
+    else {
+        string n3;
+        large = n2.length();
+        a= large-n.length();
+        for(int i=0;i<a;i++)
+            n3 = n3 +"0";
+        n=n3+n;
+
+    }
+
+    for(int i=large-1;i>-1;i--){
+
+        ans= n[i]+n2[i];
+        if(ans>10){
+            ans=ans+(i*10);
+            ans2=ans2+ans;}
+        else
+            ans=ans+(i*10);
+            ans2=ans2+ans;
+
+    }
+    cout << ans2;
 }
--- 0907 - map/14425.cpp
+++ 0907 - map/14425.cpp
@@ -1,13 +1,3 @@
-//
-// Created by 반예원 on 2021/09/13.
-//
-
-//
-// Created by 반예원 on 2021/09/13.
-//
-
-//
-
 
 #include <iostream>
 #include <vector>
@@ -29,45 +19,7 @@ int main() {
         cin >> input;
         m[input]++;
         if(m[input] > 1)
-            v.push_back(input);//
-// Created by 반예원 on 2021/09/13.
-//
-
-//
-// Created by 반예원 on 2021/09/13.
-//
-
-//
-
-
-#include <iostream>
-#include <vector>
-#include <map>
-#include <algorithm>
-        using namespace std;
-
-        int main() {
-            int n,n2,sum;
-            int cnt = 0;
-
-            map <string, int> m;
-            vector <string> v;
-
-            cin >> n >> n2;
-
-            for(int i=0;i<n+n2;i++){
-                string input;
-                cin >> input;
-                m[input]++;
-                if(m[input] > 1)
-                    v.push_back(input);
-            }
-
-
-            cout << v.size();
-
-
-        }
+            v.push_back(input);
     }
 
 
