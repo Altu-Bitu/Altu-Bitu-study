0e7fb96f2a6cff7f0a829887fa361ff45a66d330
yewonbahn <banyaewon98@gmail.com>
2021-09-13 18:24:56 +0900

    9월 13일 -Update

---
+++ 0907 - map/14425.cpp
@@ -0,0 +1,77 @@
+//
+// Created by 반예원 on 2021/09/13.
+//
+
+//
+// Created by 반예원 on 2021/09/13.
+//
+
+//
+
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n,n2,sum;
+    int cnt = 0;
+
+    map <string, int> m;
+    vector <string> v;
+
+    cin >> n >> n2;
+
+    for(int i=0;i<n+n2;i++){
+        string input;
+        cin >> input;
+        m[input]++;
+        if(m[input] > 1)
+            v.push_back(input);//
+// Created by 반예원 on 2021/09/13.
+//
+
+//
+// Created by 반예원 on 2021/09/13.
+//
+
+//
+
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+        using namespace std;
+
+        int main() {
+            int n,n2,sum;
+            int cnt = 0;
+
+            map <string, int> m;
+            vector <string> v;
+
+            cin >> n >> n2;
+
+            for(int i=0;i<n+n2;i++){
+                string input;
+                cin >> input;
+                m[input]++;
+                if(m[input] > 1)
+                    v.push_back(input);
+            }
+
+
+            cout << v.size();
+
+
+        }
+    }
+
+
+    cout << v.size();
+
+
+}
