f49412881e095191fb4e5ca1a9d9ec6531fc4fc6
yewonbahn <banyaewon98@gmail.com>
2021-09-27 17:31:44 +0900

    9월 27일 -Update

---
+++ 0921 - brute force/14501.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 반예원 on 2021/09/27.
+//
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main(){
+    int n,n2;
+    int sum=0;
+    vector<pair<int,int>> v;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        int a,b=0;
+        cin >> a >> b;
+        v.push_back(make_pair(a,b));
+    }
+    for(int i=0;i<n;i++){
+
+    while(2--){
+
+    }
+
+    }
+    cout << sum ;
+
+
+}
