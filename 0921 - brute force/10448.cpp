6521e06afa328086d57a1ef02127d01e0b3845b8
yewonbahn <banyaewon98@gmail.com>
2021-09-27 18:17:53 +0900

    9월 27일 -Update

--- 0921 - brute force/10448.cpp
+++ 0921 - brute force/10448.cpp
@@ -11,13 +11,13 @@
 #include <cmath>
 using namespace std;
 
-const int SIZE = 5000000;
+const int SIZE = 10;
 
 vector<int> isPrime() {
     vector<int> prime(SIZE + 1, 0);
 
     for(int i=1;i<prime.size();i++){
-        prime.push_back(i*(i+1)/2);
+        prime[i]=(i*(i+1)/2);
     }
 
 
@@ -28,17 +28,25 @@ int main(){
     int n,n2;
     int sum=0;
     vector<int> prime = isPrime();
+
+
     cin >> n;
+
     while(n--){
         int a;
         cin >> a;
         int sum=0;
         for(int i=0;i<prime.size();i++){
-            if(sum==a){
-                cout << 1;
-            }
-            sum=sum+prime[i];
+            for(int j=0;j<3;j++){
+                if(sum==a){
+                    cout << 1 ;
+                }
+                sum=sum+prime[i]*j;
 
+            }
+            if(sum==a){
+                cout << 1 ;
+            }
         }
 
     }
