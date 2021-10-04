af19b67c76748ceb2383ada19d4d8d575b36812a
yewonbahn <63235104+yewonbahn@users.noreply.github.com>
2021-10-04 22:15:53 +0900

    10월 4일 -Update

--- 0928 - DP/2294.cpp
+++ 0928 - DP/2294.cpp
@@ -7,21 +7,21 @@
 using namespace std;
 int knapsack_1(int n, int k, vector<int> &coin) {
     vector<int> dp(100010, 0);
-    for (int i = 0; i < k; i++) dp[i] = 2e9;
-    for (int i = 0; i < n; i++) dp[coin[i]] = 1;
+    for (int i = 1; i <= k; i++) dp[i] = 2e9;
+    for (int i = 1; i <= n; i++) dp[coin[i]] = 1;
 
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = coin[i]; j < k; j++)
+        for (int j = coin[i]; j <= k; j++)
         {
             dp[j] = min(dp[j], dp[j - coin[i]] + 1);
 
         }
     }
 
-    if (dp[k] == 2e9) cout << -1 << endl;
-    return   dp[k-1];
+    if (dp[k] == 2e9) return -1;
+    return   dp[k];
 }
 
 
@@ -29,10 +29,12 @@ int main(){
 
     int n,k;
     cin >> n >> k;
-    vector <int> coin(n,0);
-    for (int i=0;i<n;i++){
+    vector <int> coin(n+1,0);
+    for (int i=1;i<=n;i++){
         cin >> coin[i];
     }
 
     cout << knapsack_1(n, k, coin) << '\n';
+
+    return 0;
 }
