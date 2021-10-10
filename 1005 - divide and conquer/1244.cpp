2bf573994af7cd73795ed9439f93a65502a840a0
yewonbahn <63235104+yewonbahn@users.noreply.github.com>
2021-10-10 22:32:12 +0900

    10월 10일 -Update

--- 1005 - divide and conquer/1244.cpp
+++ 1005 - divide and conquer/1244.cpp
@@ -13,16 +13,25 @@ void divide(int n, int k) {
     for (int i = 0; i < k; i++) {
 
     if (v[i].first == 1) {
-        for (int j = 1; j < n; j++) {
+        for (int j = 1; j <= n; j++) {
             if (v[i].second * j > n)
                 break;
             if (v[i].second * j <= n) {
-                if (swich[v[i].second * j-1] == 0)
-                    swich[v[i].second * j-1] = 1;
-                else
-                    swich[v[i].second * j-1] = 0;
+                swich[v[i].second * j] =  !swich[v[i].second * j];
             }
         }
+    }
+    else{
+        swich[v[i].second] = !swich[v[i].second];
+        for(int j=1;j<=n;j++){
+
+            if (swich[v[i].second-j] == swich[v[i].second+j] )//
+            {
+                swich[v[i].second-j] = !swich[v[i].second-j];
+                swich[v[i].second+j] = !swich[v[i].second+j];
+            }
+        else
+            break;}
     }
 }
 }
@@ -32,8 +41,8 @@ int main() {
 
     //입력
     cin >> n; //스위치 갯수
-    swich.assign(n, 0);
-    for (int i = 0; i < n; i++) {
+    swich.assign(n+1, 0);
+    for (int i = 1; i <= n; i++) {
             cin >> swich[i];
     }
     cin >> k; //학생수
@@ -45,7 +54,7 @@ int main() {
      divide(n, k);
 
     //출력
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
         cout << swich[i] << " ";
     }
 }
