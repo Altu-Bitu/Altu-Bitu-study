84c83d038352d906f8ca28488cca41e8857a53cb
yewonbahn <63235104+yewonbahn@users.noreply.github.com>
2021-11-02 21:10:26 +0900

    11월 2일 -Update

--- 1012 - two pointer/20922.cpp
+++ 1012 - two pointer/20922.cpp
@@ -7,6 +7,9 @@
 using namespace std;
 vector<int> num2(100001,0);
 int main(){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n , n2;
     int ans=0;
     int ans3=0;
@@ -16,13 +19,18 @@ int main(){
         cin >> num[i];
 
     for(int i=0;i<n;i++) {
-        if (num2[num[i]] < n2) {
+        if (num2[num[i]] <= n2-1) {
+
             num2[num[i]]++;
             ans3++;
+
         } else {
             ans = max(ans, ans3);
+            i=i-ans3;
+            num2.assign(100001,0);
             ans3 = 0;
         }
     }
+    ans = max(ans, ans3);
     cout << ans;
 }
